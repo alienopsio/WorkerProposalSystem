43f2efacbef7c30ee61c6629b73ee792156d287e
alienops.io <alienops.io@gmail.com>
2024-12-11 23:14:58 -0600

    Add GA2

--- app/GAnalytics.tsx
+++ app/GAnalytics.tsx
@@ -1,15 +1,20 @@
+declare global {
+  interface Window {
+    gtag?: (...args: any[]) => void;
+  }
+}
+
 export const setUserProperties = (properties: Record<string, any>) => {
-    if (typeof window !== 'undefined' && window.gtag) {
-      window.gtag('set', 'user_properties', properties);
-    }
-  };
-  
-  export const trackEvent = (action: string, category: string, label: string) => {
-    if (typeof window !== 'undefined' && window.gtag) {
-      window.gtag('event', action, {
-        event_category: category,
-        event_label: label,
-      });
-    }
-  };
-  
+  if (typeof window !== 'undefined' && window.gtag) {
+    window.gtag('set', 'user_properties', properties);
+  }
+};
+
+export const trackEvent = (action: string, category: string, label: string) => {
+  if (typeof window !== 'undefined' && window.gtag) {
+    window.gtag('event', action, {
+      event_category: category,
+      event_label: label,
+    });
+  }
+};
