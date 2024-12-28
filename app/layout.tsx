785737629b3cca2a18e02f5c6cdae4cc3b761523
alienops.io <alienops.io@gmail.com>
2024-12-28 03:18:15 -0600

    Fix Constants

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ import GoogleAnalytics from './GoogleAnalytics';
 const darkerGrotesque = Darker_Grotesque({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Worker Proposal System - WPS 1.0.19",
+  title: "Worker Proposal System - WPS 1.0.20",
   description: "",
 };
 
